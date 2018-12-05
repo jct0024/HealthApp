bfb92ec2c3a537f759734c71526907e50e83f76c
Jesús <jct0024@alu.ubu.es>
2018-12-05 13:08:34 +0100

    Retoques en la base de datos
    Pequeñas modificaciones en la base de datos para una mejor entrada de datos.

--- HealthApp.py
+++ HealthApp.py
@@ -1,32 +1,43 @@
 # -*- coding: utf-8 -*-
 import openpyxl as op;
 import numpy as np;
+#Cargamos la base de datos
 doc = op.load_workbook("BaseDeDatosDeAlimentos.xlsx");
-#print(doc.sheetnames)
+print(doc.sheetnames)
+#Seleccionamos la hoja de excell que contendrá dicha información-
 hoja1 = doc['Hoja1'];
+#Inicializamos la matriz y las filas
+print(hoja1.max_row)
+print(hoja1.max_column)
 nFila=-1;
-mAlimentos = np.empty((6,8), dtype=object);
+mAlimentos = np.empty((hoja1.max_row,hoja1.max_column+1), dtype=object);
 for alimentos in hoja1:
     nFila =nFila+1
     if (nFila!=0):
         i = 0;
         for inf in alimentos:
             if (i==0):
-                mAlimentos[nFila-1,i]=inf.value;
+                mAlimentos[nFila,i]=inf.value;
             elif (i==1):
-                mAlimentos[nFila-1,i]=inf.value;
+                mAlimentos[nFila,i]=inf.value;
             elif (i==2):
-                mAlimentos[nFila-1,i]=inf.value;
+                mAlimentos[nFila,i]=inf.value;
             elif (i==3):
-                mAlimentos[nFila-1,i]=inf.value;
+                mAlimentos[nFila,i]=inf.value;
             elif (i==4):
-                mAlimentos[nFila-1,i]=inf.value;
+                mAlimentos[nFila,i]=inf.value;
             elif (i==5):
-                mAlimentos[nFila-1,i]=inf.value;
+                mAlimentos[nFila,i]=inf.value;
             elif (i==6):
-                mAlimentos[nFila-1,i]=inf.value;
+                mAlimentos[nFila,i]=inf.value;
             elif (i==7):
-                mAlimentos[nFila-1,i]=inf.value;
+                mAlimentos[nFila,i]=inf.value;
             i=i+1
+        #Iniciamos a cero la frecuencia con la que ingieren los alimentos.
+        #Esta varaible luego servirá para imlementar un LRU, para la gestión de alimentos.
+        mAlimentos[nFila,8] = 0;
+            
+print(nFila)
+print(i)
     
 print (mAlimentos)
