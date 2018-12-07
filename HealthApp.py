d6cb95c0cb39970f60b2b895b519bc49d7d6e97e
Jesús <jct0024@alu.ubu.es>
2018-12-07 10:56:36 +0100

    De Openpyxl a Pandas
    Se cambia el método de cargar la base de datos de Excel de Openpyxl a Pandas, debido a que mejora la rapidez y sencillez de la carga de datos.

--- HealthApp.py
+++ HealthApp.py
@@ -1,43 +1,17 @@
 # -*- coding: utf-8 -*-
-import openpyxl as op;
-import numpy as np;
-#Cargamos la base de datos
-doc = op.load_workbook("BaseDeDatosDeAlimentos.xlsx");
-print(doc.sheetnames)
-#Seleccionamos la hoja de excell que contendrá dicha información-
-hoja1 = doc['Hoja1'];
-#Inicializamos la matriz y las filas
-print(hoja1.max_row)
-print(hoja1.max_column)
-nFila=-1;
-mAlimentos = np.empty((hoja1.max_row,hoja1.max_column+1), dtype=object);
-for alimentos in hoja1:
-    nFila =nFila+1
-    if (nFila!=0):
-        i = 0;
-        for inf in alimentos:
-            if (i==0):
-                mAlimentos[nFila,i]=inf.value;
-            elif (i==1):
-                mAlimentos[nFila,i]=inf.value;
-            elif (i==2):
-                mAlimentos[nFila,i]=inf.value;
-            elif (i==3):
-                mAlimentos[nFila,i]=inf.value;
-            elif (i==4):
-                mAlimentos[nFila,i]=inf.value;
-            elif (i==5):
-                mAlimentos[nFila,i]=inf.value;
-            elif (i==6):
-                mAlimentos[nFila,i]=inf.value;
-            elif (i==7):
-                mAlimentos[nFila,i]=inf.value;
-            i=i+1
-        #Iniciamos a cero la frecuencia con la que ingieren los alimentos.
-        #Esta varaible luego servirá para imlementar un LRU, para la gestión de alimentos.
-        mAlimentos[nFila,8] = 0;
-            
-print(nFila)
-print(i)
-    
-print (mAlimentos)
+import pandas as pd;
+#Función que te devuelve la hoja de información de 
+#la base de datos.
+def cargarBaseDeDatos(hoja):
+    #Cargamos la base de datos
+    #doc = op.load_workbook("BaseDeDatosDeAlimentos.xlsx");  
+    doc = pd.ExcelFile("BaseDeDatosDeAlimentos.xlsx")
+    #print(doc.sheetnames) #Si añadimos hojas a la base de datos, podremos saber la información.
+    #Seleccionamos la hoja de excell que contendrá dicha información-
+    hojaExcell = pd.read_excel(doc,hoja)
+    return hojaExcell;
+
+#Cargamos la hoja1 en la cual se encuentran los alimentos con sus macronutrientes.
+hojaAlimentos = cargarBaseDeDatos('Hoja1')
+hojaAlimentos = hojaAlimentos.sort_values(by=['LRE']).sort_values(by=['Proteina'],ascending=False)
+print(hojaAlimentos)
