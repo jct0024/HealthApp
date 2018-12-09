9364930cdcdd35671fe64525e12d9df2d68e58cd
Jesús <jct0024@alu.ubu.es>
2018-12-09 23:25:53 +0100

    Inicio a avance
    Se deja preparado el programa para el siguiente paso que es distinguir entre el tipo de comida, y un menu, para el confort del usuario.

--- HealthApp.py
+++ HealthApp.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import pandas as pd;
+import numpy as np;
 #Función que te devuelve la hoja de información de 
-#la base de datos.
+#la base de datos, la cual se pasa como parametros.
 def cargarBaseDeDatos(hoja):
     #Cargamos la base de datos
     #doc = op.load_workbook("BaseDeDatosDeAlimentos.xlsx");  
@@ -11,19 +12,34 @@ def cargarBaseDeDatos(hoja):
     hojaExcell = pd.read_excel(doc,hoja)
     return hojaExcell;
 
+def guardarDatos (hoja):
+    writer = pd.ExcelWriter("BaseDeDatosDeAlimentos.xlsx")
+    hojaAlimentos.to_excel(writer,hoja)
+    writer.save();
+ a = -1;   
+while (a !=0):
+   #Hacer todo lo del menu , opciones apra el cliente y demas
+   a=0;
+#Vectores datos alimentos: calorias/grasas/garasas saturadas/hidratos/azucares/proteina
+datosAlimCliente = np.zeros(6)
+datosAlimTotal = np.zeros(6)
 #Cargamos la hoja1 en la cual se encuentran los alimentos con sus macronutrientes.
 hojaAlimentos = cargarBaseDeDatos('Hoja1')
-print(hojaAlimentos)
 hojaAlimentos = hojaAlimentos.sort_values(by=['Proteina'],ascending=False).sort_values(by=['LRE'])
 n_Opciones = 3;
-for i in range(n_Opciones):
-    print(i, " ",hojaAlimentos["Nombre"].iloc[i])
-opc = int(input("Introduzca la opción que desea desayunar"))
-hojaAlimentos.loc[opc,'LRE'] =hojaAlimentos.loc[opc,'LRE'] + 1
-hojaAlimentos = hojaAlimentos.sort_values(by=['Proteina'],ascending=False).sort_values(by=['LRE'])
-writer = pd.ExcelWriter("BaseDeDatosDeAlimentos.xlsx")
-hojaAlimentos.to_excel(writer,'Hoja1')
-writer.save();
+#Escoger entre opciones el desayuno de hoy:
+while (datosAlimCliente[0] != datosAlimTotal):
+    for i in range(n_Opciones):
+        print(i, " ",hojaAlimentos["Nombre"].iloc[i])
+        opc = int(input("Introduzca la opción que desea desayunar"))
+        hojaAlimentos.iloc[opc,8] =hojaAlimentos.iloc[opc,8] + 1
+
+    #Rellenar datos.
+    for i in range(datosAlimTotal.size):
+        datosAlimCliente[i] = hojaAlimentos.iloc[opc,i+1] + datosAlimCliente[i]
+    print(datosAlimCliente)
+    hojaAlimentos = hojaAlimentos.sort_values(by=['Proteina'],ascending=False).sort_values(by=['LRE'])
+guardarDatos ('Hoja1')
 print(hojaAlimentos)
 PRUEBA = str(1101);
 total=0;
