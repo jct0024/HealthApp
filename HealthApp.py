adfcd5000124865d9134e914b7153a62701ba98e
Jesús <jct0024@alu.ubu.es>
2018-12-10 20:15:08 +0100

    Registro 1.0
    Se empieza a trabajar con usuarios y se crea un registro sencillo para esto.

--- HealthApp.py
+++ HealthApp.py
@@ -1,48 +1,46 @@
-# -*- coding: utf-8 -*-
-import pandas as pd;
+# -*- coding: utf-8 -*-1
 import numpy as np;
-#Función que te devuelve la hoja de información de 
-#la base de datos, la cual se pasa como parametros.
-def cargarBaseDeDatos(hoja):
-    #Cargamos la base de datos
-    #doc = op.load_workbook("BaseDeDatosDeAlimentos.xlsx");  
-    doc = pd.ExcelFile("BaseDeDatosDeAlimentos.xlsx")
-    #print(doc.sheetnames) #Si añadimos hojas a la base de datos, podremos saber la información.
-    #Seleccionamos la hoja de excell que contendrá dicha información-
-    hojaExcell = pd.read_excel(doc,hoja)
-    return hojaExcell;
-
-def guardarDatos (hoja):
-    writer = pd.ExcelWriter("BaseDeDatosDeAlimentos.xlsx")
-    hojaAlimentos.to_excel(writer,hoja)
-    writer.save();
- a = -1;   
-while (a !=0):
-   #Hacer todo lo del menu , opciones apra el cliente y demas
-   a=0;
-#Vectores datos alimentos: calorias/grasas/garasas saturadas/hidratos/azucares/proteina
+import AdminBase as ab
+#Función que carga la base de datos.
 datosAlimCliente = np.zeros(6)
 datosAlimTotal = np.zeros(6)
+flagMenu = -1;
+flagLogIn = False
+#Vectores datos alimentos: calorias/grasas/garasas saturadas/hidratos/azucares/proteina
 #Cargamos la hoja1 en la cual se encuentran los alimentos con sus macronutrientes.
-hojaAlimentos = cargarBaseDeDatos('Hoja1')
-hojaAlimentos = hojaAlimentos.sort_values(by=['Proteina'],ascending=False).sort_values(by=['LRE'])
-n_Opciones = 3;
-#Escoger entre opciones el desayuno de hoy:
-while (datosAlimCliente[0] != datosAlimTotal):
-    for i in range(n_Opciones):
-        print(i, " ",hojaAlimentos["Nombre"].iloc[i])
-        opc = int(input("Introduzca la opción que desea desayunar"))
-        hojaAlimentos.iloc[opc,8] =hojaAlimentos.iloc[opc,8] + 1
+hojaAlimentos, hojaUsuarios, hojaPatologias = ab.cargarBaseDeDatos()
+#print(hojaAlimentos)
+while ( flagLogIn == False):
+    user = int(input("Introduzca el DNI"))
+    pwd = int(input("Introduzca la contraseña"))
+    if (ab.comprobarUsuario(user,pwd)):
+        print("Bienvenido ", hojaUsuarios.iloc[ab.getFilaUsuario(user),1])
+        flagLogIn = True
+    else:
+        print("Usuario o contraseña erroneos vuelva a intentarlo")
+while (flagMenu !=0):
+   #Hacer todo lo del menu , opciones para el cliente y demas
+   print('1: Mostrar datos del usuario' )
+   print('2: Mostrar dieta de hoy')
+   print('0: Salir del programa')
+   flagMenu = int(input("Introduzca la opción que desea hacer hoy"))
+   if (flagMenu==1):
+       #Datos del usuario.
+       break;
+   elif (flagMenu ==2):
+       hojaAlimentos = hojaAlimentos.sort_values(by=['Proteina'],ascending=False).sort_values(by=['LRE'])
+       n_Opciones = 3;
+       #Escoger entre opciones el desayuno de hoy:
+       while (datosAlimCliente[0] != datosAlimTotal[0]):
+           for i in range(n_Opciones):
+                print(i, " ",hojaAlimentos["Nombre"].iloc[i])
+                opc = int(input("Introduzca la opción que desea desayunar"))
+                hojaAlimentos.iloc[opc,8] =hojaAlimentos.iloc[opc,8] + 1
+        #Rellenar datos.
+       for i in range(datosAlimTotal.size):
+          datosAlimCliente[i] = hojaAlimentos.iloc[opc,i+1] + datosAlimCliente[i]
+          print(datosAlimCliente)
+          hojaAlimentos = hojaAlimentos.sort_values(by=['Proteina'],ascending=False).sort_values(by=['LRE'])
 
-    #Rellenar datos.
-    for i in range(datosAlimTotal.size):
-        datosAlimCliente[i] = hojaAlimentos.iloc[opc,i+1] + datosAlimCliente[i]
-    print(datosAlimCliente)
-    hojaAlimentos = hojaAlimentos.sort_values(by=['Proteina'],ascending=False).sort_values(by=['LRE'])
-guardarDatos ('Hoja1')
-print(hojaAlimentos)
-PRUEBA = str(1101);
-total=0;
-for p in PRUEBA:
-    total=int(p)+total
-    print(total)
+ab.guardarDatos (hojaAlimentos,hojaUsuarios,hojaPatologias)
+print("Ya puede cerrar el sistema, hasta pronto:)")
